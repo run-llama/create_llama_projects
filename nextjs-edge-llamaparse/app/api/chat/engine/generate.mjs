738f1cf393dae2c77abbe1f1a65b79899275968a
Marcus Schiesser <mail@marcusschiesser.de>
2024-03-25 12:06:05 +0800

    chore: added prettier

--- nextjs-edge-llamaparse/app/api/chat/engine/generate.mjs
+++ nextjs-edge-llamaparse/app/api/chat/engine/generate.mjs
@@ -9,21 +9,23 @@ import { checkRequiredEnvVars } from "./shared.mjs";
 dotenv.config();
 
 async function loadAndIndex() {
-	// load objects from storage and convert them into LlamaIndex Document objects
-	const documents = await getDocuments();
+  // load objects from storage and convert them into LlamaIndex Document objects
+  const documents = await getDocuments();
 
-	// create vector store
-	const vectorStore = new PineconeVectorStore();
+  // create vector store
+  const vectorStore = new PineconeVectorStore();
 
-	// create index from all the Documentss and store them in Pinecone
-	console.log("Start creating embeddings...");
-	const storageContext = await storageContextFromDefaults({ vectorStore });
-	await VectorStoreIndex.fromDocuments(documents, { storageContext });
-	console.log("Successfully created embeddings and save to your Pinecone index.");
+  // create index from all the Documentss and store them in Pinecone
+  console.log("Start creating embeddings...");
+  const storageContext = await storageContextFromDefaults({ vectorStore });
+  await VectorStoreIndex.fromDocuments(documents, { storageContext });
+  console.log(
+    "Successfully created embeddings and save to your Pinecone index.",
+  );
 }
 
 (async () => {
-	checkRequiredEnvVars();
-	await loadAndIndex();
-	console.log("Finished generating storage.");
+  checkRequiredEnvVars();
+  await loadAndIndex();
+  console.log("Finished generating storage.");
 })();
